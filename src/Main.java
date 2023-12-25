c433bbe75dc9cac00c03b6f9c588c172734c658a
User <nazirovadilzoda2000@gmail.com>
2023-12-25 17:32:46 +0500

    askmamska

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Assalomu alaykum Yusufbek");
+        System.out.println("Assalomu alaykum Yusufbek!");
     }
+
+
+    public void add(){}
 }
