7dedf70ffa3b8bbb45cf44743492ccd843ac483e
SuhrobPDP <hosiljonov00@gmail.com>
2024-01-08 17:48:40 +0500

    salom check

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Assalomu alaykum Yusufbek!");
         System.out.println("Assalomu alaykum suhrob!");
+
+
+
+
     }
 
 
